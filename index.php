2475c5aac45139afdf464bcdb4c6f4add5538cdf
Perepelytsia <perepelytsia1988@gmail.com>
2017-02-24 03:42:23 +0200

    проверка на драйвера SQLite

--- index.php
+++ index.php
@@ -1,5 +1,8 @@
 <?php
-require_once('app.php');
-$app=new App();
-$app->run();
-?>
+if (extension_loaded('pdo_sqlite')) {
+    require_once('app.php');
+    $app=new App();
+    $app->run();
+} else {
+    echo '<h1>PDO extension or SQLite PRO driver not loaded.</h1>';
+}
